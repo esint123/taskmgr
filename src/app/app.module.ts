eab490f272754a22984c3f02e13dd52c7b12bbd3
admin <aa>
2018-03-20 20:08:39 +0800

    s

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
 
-
-import { AppComponent } from './app.component';
-
+import {CoreModule} from './core/core.module';
+import {SharedModule} from './shared/shared.module';
+import {LoginModule} from './login/login.module';
+import {ProjectModule} from './project/project.module';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CoreModule,
+    SharedModule,
+    LoginModule,
+    ProjectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
